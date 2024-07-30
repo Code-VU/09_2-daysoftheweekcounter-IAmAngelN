c1317987fef6fb0372102fab3e02093b8da36a2e
ItsAngelN <iAmAngelN@iCloud.Com>
2024-07-30 00:35:38 -0500

    opened a text file and split lines starting with from, then had the third word added to a dictionary along with how many times it appeard with a loop. I had to add lens(words) to account for the uncomplete lines that didnt include dates in the text file. That index error stumped me for a while

--- counter.py
+++ counter.py
@@ -2,6 +2,26 @@ def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
 
+    fh = open(file_name)
+
+    lines = fh.readlines()
+
+    weekday = dict()
+
+    for line in lines:
+        if line.startswith("From "):
+            words = line.split()
+            if len(words) > 3:
+                dow = words[2]
+                if dow not in weekday:
+                    weekday[dow] = 1
+                else:
+                    weekday[dow] += 1
+
+    print(weekday)
+
+            
+
 
 
 ## if you want to test locally run > python payCalculator.py
